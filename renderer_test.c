f93d62b748a915d5cc4f7707ded61feb246c15db
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-02-04 15:43:39 -0700

    Stubbed renderer test

---
+++ renderer_test.c
@@ -0,0 +1,22 @@
+/** !
+ * Tester for renderer 
+ * 
+ * @file renderer_test.c
+ * 
+ * @author Jacob Smith
+ */
+
+// Header files
+#include <stdio.h>
+#include <stdlib.h>
+
+// Entry point
+int main ( int argc, const char *argv[] )
+{
+
+    // TODO
+    printf("[renderer] The renderer tester has not yet been implemented!\n");
+
+    // Success
+    return EXIT_SUCCESS;
+}
