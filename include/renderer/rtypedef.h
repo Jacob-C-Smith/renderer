2633ef89423854c396ada9d057e18d934d954338
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-02-04 15:43:11 -0700

    Created renderer typedef

---
+++ include/renderer/rtypedef.h
@@ -0,0 +1,24 @@
+/** ! 
+ * Type definitions for renderer
+ * 
+ * @file renderer/rtypedef.h
+ * 
+ * @author Jacob Smith
+ */
+
+// Include guard
+#pragma once
+
+// Forward declarations
+struct camera_s;
+struct primitive_s;
+struct shape_s;
+struct spectrum_s;
+struct integrator_s;
+
+// Type definitions
+typedef struct camera_s camera;
+typedef struct primitive_s primitive;
+typedef struct shape_s shape;
+typedef struct spectrum_s spectrum;
+typedef struct integrator_s integrator;
