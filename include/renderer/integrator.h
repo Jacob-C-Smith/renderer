22285e79468294d69824163d08e924c3ae4d6e3d
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-03-24 11:55:09 -0600

    [integrator] Added some code

--- include/renderer/integrator.h
+++ include/renderer/integrator.h
@@ -10,10 +10,27 @@
 #pragma once
 
 // Header files
+#include <renderer/rtypedef.h>
 #include <renderer/primitive.h>
+#include <renderer/light.h>
+
+// Preprocessor definitions
+#define RENDERER_INTEGRATOR_MAX_LIGHTS
 
 // Structure definitions
 struct integrator_s
 {
     primitive *p_aggregate;
+    light     *p_lights[RENDERER_INTEGRATOR_MAX_LIGHTS];
 };
+
+// Function declarations
+/** !
+ * Construct an integrator from a primitive aggregate and a list of lights
+ * 
+ * @param p_aggregate the scene geometry
+ * @param pp_lights   a null pointer terminated list of lights
+ * 
+ * @return 1 on success, 0 on error
+ */
+int renderer_integrator_construct ( integrator *p_integrator, primitive *p_aggregate, light **pp_lights );
